b3568bc64fdfa1637f18aff36935d5a9d2a9f486
rancheng <rancheng@nuiro.com>
2024-07-09 11:27:34 +0800

    1

--- src/components/node.vue
+++ src/components/node.vue
@@ -4,13 +4,45 @@ defineProps({
   name: String
 })
 
-const nodeDetail = await new Promise((resolve)=>{
-  setTimeout(()=>{
-    resolve({
-      users: ['rancheng']
-    })
-  },100)
-})
+// const nodeDetail = await new Promise((resolve)=>{
+//   setTimeout(()=>{
+//     resolve({
+//       users: ['rancheng']
+//     })
+//   },100)
+// })
+
+var startTime = Date.now()
+const results = await Promise.all([
+  new Promise((resolve)=>{
+    setTimeout(()=>{
+      resolve({
+        users: ['rancheng']
+      })
+    },100)
+  }),
+  new Promise((resolve)=>{
+    setTimeout(()=>{
+      resolve({
+        age: 18
+      })
+    },100)
+  }),
+  new Promise((resolve)=>{
+    setTimeout(()=>{
+      resolve({
+        age: 18
+      })
+    },100)
+  })
+])
+var endTime = Date.now()
+
+console.log('rangeTime',endTime - startTime)
+
+const nodeDetail = results[0]
+const userDetail = results[1]
+
 //console.log('node loaded')
 
 </script>
@@ -19,5 +51,6 @@ const nodeDetail = await new Promise((resolve)=>{
   <h1>
     {{name}}
     {{nodeDetail.users[0]}}
+    {{userDetail.age}}
   </h1>
 </template>
